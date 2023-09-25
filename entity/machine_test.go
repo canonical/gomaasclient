7b368b22d48f7c70424147713b0de0f329f03815
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2023-09-25 12:19:42 +0300

    Update machine struct fields

--- entity/machine_test.go
+++ entity/machine_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/maas/gomaasclient/test/helper"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMachinet(t *testing.T) {
@@ -98,4 +99,46 @@ func TestMAASTimeUnmarshalJSON(t *testing.T) {
 	if temp.Time.String() != expected {
 		t.Fatalf("expected %v, got %v", expected, temp.Time.String())
 	}
+}
+
+func TestMAASUnMarshal(t *testing.T) {
+	machine := new(Machine)
+
+	// Unmarshal sample data into the types
+	if err := helper.TestdataFromJSON("maas/machine.json", machine); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("CacheSets", func(t *testing.T) {
+		assert.Equal(t, 1, len(machine.CacheSets))
+		assert.Equal(t, 5, machine.CacheSets[0].ID)
+		assert.Equal(t, true, machine.CacheSets[0].Incomplete)
+		assert.Equal(t, "dqatxp", machine.CacheSets[0].SystemID)
+	})
+	t.Run("VolumeGroups", func(t *testing.T) {
+		assert.Equal(t, 1, len(machine.VolumeGroups))
+		assert.Equal(t, 4, machine.VolumeGroups[0].ID)
+		assert.Equal(t, true, machine.VolumeGroups[0].Incomplete)
+		assert.Equal(t, "dqatxp", machine.VolumeGroups[0].SystemID)
+	})
+	t.Run("BCaches", func(t *testing.T) {
+		assert.Equal(t, 1, len(machine.BCaches))
+		assert.Equal(t, 3, machine.BCaches[0].ID)
+		assert.Equal(t, true, machine.BCaches[0].Incomplete)
+		assert.Equal(t, "3gf1k4", machine.BCaches[0].SystemID)
+	})
+	t.Run("RAIDs", func(t *testing.T) {
+		assert.Equal(t, 1, len(machine.RAIDs))
+		assert.Equal(t, 2, machine.RAIDs[0].ID)
+		assert.Equal(t, true, machine.RAIDs[0].Incomplete)
+		assert.Equal(t, "rdt4fa", machine.RAIDs[0].SystemID)
+	})
+	t.Run("SpecialFS", func(t *testing.T) {
+		assert.Equal(t, 1, len(machine.SpecialFilesystems))
+		assert.Equal(t, "ramfs", machine.SpecialFilesystems[0].FSType)
+		assert.Equal(t, "special", machine.SpecialFilesystems[0].Label)
+		assert.Equal(t, "0e8b2c73-c31f-4fd0-b3b9-8dde74406afe", machine.SpecialFilesystems[0].UUID)
+		assert.Equal(t, "/MqbWGqzmWV/KrM409CMFO/CsDtuVxjpQ", machine.SpecialFilesystems[0].MountPoint)
+		assert.Equal(t, "mount-options-552Rua", machine.SpecialFilesystems[0].MountOptions)
+	})
 }
