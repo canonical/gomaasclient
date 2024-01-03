6353a210457397af91c86321502125d821f9f03b
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2024-01-03 09:02:29 +0200

    bug: resource pool create return pool object (#39)

--- api/resource_pools.go
+++ api/resource_pools.go
@@ -5,5 +5,5 @@ import "github.com/maas/gomaasclient/entity"
 // ResourcePools is an interface for listing and creating ResourcePool records
 type ResourcePools interface {
 	Get() ([]entity.ResourcePool, error)
-	Create(params *entity.ResourcePoolParams) (*entity.Domain, error)
+	Create(params *entity.ResourcePoolParams) (*entity.ResourcePool, error)
 }
