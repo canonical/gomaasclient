22159d66465b5446ecb9be4a34105622add53217
sachin shingare <35319163+sachin120@users.noreply.github.com>
2024-02-13 17:13:19 +0530

    feat: add numanode info to machine object (#61)
    Signed-off-by: Sachin Shingare <doitqik@gmail.com>

--- entity/node.go
+++ entity/node.go
@@ -2,3 +2,19 @@ package entity
 
 // Node represents the MAAS Node endpoint.
 type Node Machine
+
+// NUMANode represents the MAAS numa node
+// referred from https://github.com/maas/maas/blob/deab73792a4fe839a2e84a926a6c728d510fc9ad/src/maasserver/api/machines.py#L110
+type NUMANode struct {
+	HugepagesSet []NUMANodeHugepages `json:"hugepages_set,omitempty"`
+	Cores        []int               `json:"cores,omitempty"`
+	Index        int                 `json:"index,omitempty"`
+	Memory       int                 `json:"memory,omitempty"`
+}
+
+// NUMANodeHugepages represents the MAAS numa node hugepages
+// referred from https://github.com/maas/maas/blob/deab73792a4fe839a2e84a926a6c728d510fc9ad/src/maasserver/api/machines.py#L108
+type NUMANodeHugepages struct {
+	PageSize int `json:"page_size,omitempty"`
+	Total    int `json:"total,omitempty"`
+}
