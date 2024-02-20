3887b493ad864a8be5e683e5f435bb059941fa1c
sachin shingare <35319163+sachin120@users.noreply.github.com>
2024-02-20 16:03:31 +0530

    feat: Add node device discovery API endpoint interaction (#57)
    Signed-off-by: Sachin Shingare <doitqik@gmail.com>

---
+++ api/discoveries.go
@@ -0,0 +1,12 @@
+package api
+
+import "github.com/maas/gomaasclient/entity"
+
+type Discoveries interface {
+	Get() ([]entity.Discovery, error)
+	GetByUnknownIP() ([]entity.Discovery, error)
+	GetByUnknownMAC() ([]entity.Discovery, error)
+	GetByUnknownIpAndMAC() ([]entity.Discovery, error)
+	Clear(params *entity.DiscoveryClearParams) error
+	ClearByMACAndIP(mac, ip string) error
+}
