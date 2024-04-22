3daa832d9a3c839be69843cef0359ce892807698
sachin shingare <35319163+sachin120@users.noreply.github.com>
2024-04-22 20:11:39 +0530

    feat: Add commissioning result API endpoint interaction (#64)
    Signed-off-by: Sachin Shingare <doitqik@gmail.com>

---
+++ client/node_results.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"encoding/json"
+
+	"github.com/google/go-querystring/query"
+	"github.com/maas/gomaasclient/entity"
+)
+
+// NodeResults implements api.NodeResults
+type NodeResults struct {
+	APIClient APIClient
+}
+
+func (c *NodeResults) client() APIClient {
+	return c.APIClient.GetSubObject("installation-results")
+}
+
+// Get node results
+func (c *NodeResults) Get(params *entity.NodeResultParams) ([]entity.NodeResult, error) {
+	qsp, err := query.Values(params)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]entity.NodeResult, 0)
+	err = c.client().Get("", qsp, func(data []byte) error {
+		return json.Unmarshal(data, &results)
+	})
+
+	return results, err
+}
