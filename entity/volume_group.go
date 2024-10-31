374a4df660ecf998b6f58c8cd11e602b1596f3ba
Moto Ishizawa <summerwind.jp@gmail.com>
2024-11-01 08:14:29 +0900

    Add VirtualBlockDevice

--- entity/volume_group.go
+++ entity/volume_group.go
@@ -2,19 +2,19 @@ package entity
 
 // VolumeGroup represents the MAAS VolumeGroup endpoint.
 type VolumeGroup struct {
-	Devices            interface{}   `json:"devices,omitempty"`
-	ResourceURI        string        `json:"resource_uri,omitempty"`
-	HumanSize          string        `json:"human_size,omitempty"`
-	UUID               string        `json:"uuid,omitempty"`
-	HumanAvailableSize string        `json:"human_available_size,omitempty"`
-	SystemID           string        `json:"system_id,omitempty"`
-	HumanUsedSize      string        `json:"human_used_size,omitempty"`
-	Name               string        `json:"name,omitempty"`
-	LogicalVolumes     []BlockDevice `json:"logical_volumes,omitempty"`
-	Size               int64         `json:"size,omitempty"`
-	UsedSize           int64         `json:"used_size,omitempty"`
-	AvailableSize      int64         `json:"available_size,omitempty"`
-	ID                 int           `json:"id,omitempty"`
+	Devices            interface{}          `json:"devices,omitempty"`
+	ResourceURI        string               `json:"resource_uri,omitempty"`
+	HumanSize          string               `json:"human_size,omitempty"`
+	UUID               string               `json:"uuid,omitempty"`
+	HumanAvailableSize string               `json:"human_available_size,omitempty"`
+	SystemID           string               `json:"system_id,omitempty"`
+	HumanUsedSize      string               `json:"human_used_size,omitempty"`
+	Name               string               `json:"name,omitempty"`
+	LogicalVolumes     []VirtualBlockDevice `json:"logical_volumes,omitempty"`
+	Size               int64                `json:"size,omitempty"`
+	UsedSize           int64                `json:"used_size,omitempty"`
+	AvailableSize      int64                `json:"available_size,omitempty"`
+	ID                 int                  `json:"id,omitempty"`
 }
 
 // VolumeGroupCreateParams enumerates the parameters for the volume group create operation.
@@ -35,9 +35,14 @@ type VolumeGroupUpdateParams struct {
 	RemovePartitions   []string `url:"remove_partitions,omitempty"`
 }
 
-// LogicalVolumeParams enumerates the parameters for the logical volume operation
+// LogicalVolumeParams enumerates the parameters for the logical volume operation.
 type LogicalVolumeParams struct {
 	Name string `url:"name,omitempty"`
 	UUID string `url:"uuid,omitempty"`
 	Size int64  `json:"size,omitempty"`
+}
+
+// VirtualBlockDevice represents a logical volume and extends BlockDevice.
+type VirtualBlockDevice struct {
+	BlockDevice
 }
