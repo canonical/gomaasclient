9f11e0383ca2beeb4758f8e4b5cc2897fcac9615
seb54000 <seb54000@gmail.com>
2025-04-13 14:07:53 +0200

    fix structures for static routes

--- entity/static_route.go
+++ entity/static_route.go
@@ -2,15 +2,15 @@ package entity
 
 type StaticRoute struct {
 	ID          int    `json:"id,omitempty"`
-	Source      string `json:"source,omitempty"`
-	Destination string `json:"destination,omitempty"`
+	Source      Subnet `json:"source,omitempty"`
+	Destination Subnet `json:"destination,omitempty"`
 	Gateway_ip  string `json:"gateway_ip,omitempty"`
-	Metric      string `json:"metric,omitempty"`
+	Metric      int    `json:"metric,omitempty"`
 }
 
 type StaticRouteParams struct {
 	Source      string `json:"source,omitempty"`
 	Destination string `json:"destination,omitempty"`
 	Gateway_ip  string `json:"gateway_ip,omitempty"`
-	Metric      string `json:"metric,omitempty"`
+	Metric      int    `json:"metric,omitempty"`
 }
