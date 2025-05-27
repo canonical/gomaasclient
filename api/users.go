4c7b3bc397cb6072c7e5300ec1679e88dbbbf66f
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-05-27 17:55:06 +0100

    whoami gives one user

--- api/users.go
+++ api/users.go
@@ -6,5 +6,5 @@ import "github.com/canonical/gomaasclient/entity"
 type Users interface {
 	Get() ([]entity.User, error)
 	Create(params *entity.UserParams) (*entity.User, error)
-	Whoami() ([]entity.User, error)
+	Whoami() (*entity.User, error)
 }
