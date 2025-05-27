536dac85d09ec37892305ffc862f21955dd475ad
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-05-27 17:24:20 +0100

    add whoami

--- api/user.go
+++ api/user.go
@@ -5,5 +5,5 @@ import "github.com/canonical/gomaasclient/entity"
 // User is an interface for getting and deleting users
 type User interface {
 	Get(userName string) (*entity.User, error)
-	Delete(userName string) error
+	Delete(params *entity.UserDeleteParams) error
 }
